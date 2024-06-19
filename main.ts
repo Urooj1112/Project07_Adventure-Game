6cd4c89b12c845968ca8a7a88f76078bc8f3d625
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-06-19 01:47:36 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,124 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+class Player {
+  name: string;
+  fuel: number = 100;
+  constructor(name: string) {
+    this.name = name;
+  }
+  fuelDecrease() {
+    let fuel = this.fuel - 25;
+    this.fuel = fuel;
+  }
+  fuelIncrease() {
+    this.fuel = 100;
+  }
+}
+class Opponent {
+  name: string;
+  fuel: number = 100;
+  constructor(name: string) {
+    this.name = name;
+  }
+  fuelDecrease() {
+    let fuel = this.fuel - 25;
+    this.fuel = fuel;
+  }
+}
+let player = await inquirer.prompt([
+  {
+    name: "name",
+    type: "input",
+    message: "Please enter your name: ",
+  },
+]);
+
+let opponent = await inquirer.prompt([
+  {
+    name: "select",
+    type: "list",
+    message: "Select your opponent",
+    choices: ["Skeleton", "Alien", "Zombie"],
+  },
+]);
+
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent.select);
+
+do {
+  if (opponent.select == "Skeleton") {
+    let ask = await inquirer.prompt([
+      {
+        name: "opt",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Attack", "Drink portion", "Run for your life..."],
+      },
+    ]);
+
+    if (ask.opt == "Attack") {
+      let num = Math.floor(Math.random() * 2);
+      if (num > 0) {
+        p1.fuelDecrease();
+        // console.log(`${p1.name} attacked ${o1.name}! Successful hit!`);
+        console.log(`${p1.name} fuel is ${p1.fuel}`);
+        console.log(`${o1.name} fuel is ${o1.fuel}`);
+        if (p1.fuel <= 0) {
+          console.log("You loose better luck next time");
+          process.exit();
+        }
+      }
+      if (num <= 0) {
+        o1.fuelDecrease();
+        // console.log(`${p1.name} attacked ${o1.name}! Successful hit!`);
+        console.log(`${p1.name} fuel is ${p1.fuel}`);
+        console.log(`${o1.name} fuel is ${o1.fuel}`);
+        if (o1.fuel <= 0) {
+          console.log("You win");
+          process.exit();
+        }
+      }
+    }
+    if (ask.opt == "Drink portion") {
+      p1.fuelIncrease();
+      console.log("You drink health portion your fuel is " + p1.fuel);
+      
+      
+      
+      let num = Math.floor(Math.random() * 2);
+      if (num > 0) {
+        p1.fuelDecrease();
+        console.log(`${p1.name} attacked ${o1.name}! Successful hit!`);
+        console.log(`${p1.name} fuel is ${p1.fuel}`);
+        console.log(`${o1.name} fuel is ${o1.fuel}`);
+
+        if (num <= 0) {
+          o1.fuelDecrease();
+          console.log(`${p1.name} attacked ${o1.name}! Successful hit!`);
+          console.log(`${p1.name} fuel is ${p1.fuel}`);
+          console.log(`${o1.name} fuel is ${o1.fuel}`);
+        }
+       }
+       else {
+        console.log(`${p1.name} attacked ${o1.name}, but missed!`);
+        p1.fuelDecrease(); // Your fuel decreases if attack is not successful
+        console.log(`${p1.name} fuel is ${p1.fuel}`);
+        console.log(`${o1.name} fuel is ${o1.fuel}`);
+
+        if (p1.fuel <= 0) {
+          console.log("You loose, better luck next time");
+          process.exit();
+        }
+      }
+
+
+
+
+    }
+    if (ask.opt == "Run for your life...") {
+      console.log("You loose, better luck next time");
+      process.exit();
+    }
+  }
+} while (true);
